f0533ca813728c329a8ca9dfe4eb71b0a9e3ba19
Smit-Kotadiya <smitkotadiya01@gmail.com>
2025-06-04 22:07:21 +0200

    Further Modifications

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,11 @@ app.use("/notes", notesRouter);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => {
+  console.error("MongoDB connection failed", err);
+  process.exit(1);
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
